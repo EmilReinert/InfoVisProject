e1f39a2fee2cb60d83ff33b809845094ac4117ea
Emil Reinert <EmilReinert>
2019-08-15 12:49:51 +0200

    Setup Meshmaker for first Image Output based on "view" class generated pixels

--- src/infomesh/MeshMaker.java
+++ src/infomesh/MeshMaker.java
@@ -1,11 +1,20 @@
 package infomesh;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import java.io.File;
-import java.util.ArrayList;
+import javax.swing.JFrame;
+
 
 
 public class MeshMaker extends JFrame implements Runnable {
+	
+	//For Window pixle size
+	public static int WIDTH = 800;
+	public static int HEIGHT = 800;	
 	
 	private Thread thread;
 	private boolean running;
@@ -14,14 +23,70 @@ public class MeshMaker extends JFrame implements Runnable {
 	public Camera camera;
 	public View view; //= Screen
 	
+	public MeshMaker() {
+		thread = new Thread(this);
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		pixels = pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+		camera = new Camera(4.5, 4.5, 1, 0, 0, -.66);
+		view = new View(WIDTH,HEIGHT);
+		setSize(WIDTH, HEIGHT);
+		setTitle("InfoMesh");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(Color.black);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		start();
+	}
 	
-	public static void main(String[] args) {
-		Model m = new Model(new File("data/Data_Mortality.txt"));
+	private synchronized void start() {
+		running = true;
+		thread.start();
+	}	
+	
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+		bs.show();
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		long lastTime = System.nanoTime();
+		final double ns = 1000000000.0 / 60.0;//60 times per second
+		double delta = 0;
+		requestFocus();
+		while(running) {
+			long now = System.nanoTime();
+			delta = delta + ((now-lastTime) / ns);
+			lastTime = now;
+			while (delta >= 1)//Make sure update is only happening 60 times a second
+			{
+				//handles all of the logic restricted time
+				view.update(camera, pixels);
+				delta--;
+			}
+			render();//displays to the screen unrestricted time
+		}
 	}
+	
+	public static void main(String[] args) {
+		Model m = new Model(new File("data/Data_Mortality.txt"));
+		MeshMaker mesh = new MeshMaker();
+	}
+
+
 }
